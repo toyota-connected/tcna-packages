fd2d3a5fe1627bd24ee2662f313bca7ad1e3c0e3
Jamie Kerber <kerber.jg@gmail.com>
2025-04-25 16:49:30 +0200

    [filament_scene] Implemented new transform/vector support

--- packages/filament_scene/lib/math/vectors.dart
+++ packages/filament_scene/lib/math/vectors.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:vector_math/vector_math.dart';
-export 'package:vector_math/vector_math.dart' show Vector2, Vector3, Quaternion;
+export 'package:vector_math/vector_math.dart' show Vector2, Vector3, Quaternion, Matrix4;
 
 extension Vector3WithExtras on Vector3 {
+  /// Construct a [Vector3] from a [Vector3Data].
+  static Vector3 fromData(final Vector3Data data) => Vector3(data.x, data.y, data.z);
+
   /// Multiply each component of the vector by the corresponding component of the other vector.
   Vector3 mul(final Vector3 other) => Vector3(x * other.x, y * other.y, z * other.z);
 
@@ -9,6 +13,23 @@ extension Vector3WithExtras on Vector3 {
 }
 
 extension QuaterionWithDegrees on Quaternion {
+  /// Construct a [Quaternion] from [QuaternionData].
+  static Quaternion fromData(final QuaternionData data) {
+    final Quaternion q = Quaternion.identity();
+
+    if (data.isEuler) {
+      if (data.isDegrees) {
+        q.setEulerDegrees(data.x, data.y, data.z);
+      } else {
+        q.setEulerRadians(data.x, data.y, data.z);
+      }
+    } else {
+      q.setValues(data.x, data.y, data.z, data.w);
+    }
+
+    return q;
+  }
+
   void setEulerDegrees(final double x, final double y, final double z) {
     setEuler(y * degrees2Radians, x * degrees2Radians, z * degrees2Radians);
   }
@@ -18,6 +39,59 @@ extension QuaterionWithDegrees on Quaternion {
   }
 }
 
+/// A data class for Vector3
+@immutable class Vector3Data {
+  final double x, y ,z;
+
+  const Vector3Data({
+    required this.x,
+    required this.y,
+    required this.z,
+  });
+
+  @override
+  String toString() => 'Vector3Data(x: $x, y: $y, z: $z)';
+}
+
+/// A data class for Quaternion
+@immutable class QuaternionData {
+  final double x, y, z, w;
+  final bool isEuler, isDegrees;
+
+  const QuaternionData({
+    required this.x,
+    required this.y,
+    required this.z,
+    required this.w,
+  }) :
+    isEuler = false,
+    isDegrees = false;
+
+  /// Euler angles in radians
+  const QuaternionData.euler({
+    required this.x,
+    required this.y,
+    required this.z,
+  }) : 
+    w = double.nan,
+    isEuler = true,
+    isDegrees = false;
+
+  /// Euler angles in degrees
+  const QuaternionData.eulerDegrees({
+    required this.x,
+    required this.y,
+    required this.z,
+  }) : 
+    w = double.nan,
+    isEuler = true,
+    isDegrees = true;  
+  
+
+  @override
+  String toString() => 'QuaternionData(x: $x, y: $y, z: $z, w: $w)';
+}
+
 typedef Position = Vector3;
 typedef Size = Vector3;
 typedef Scale = Vector3;
@@ -25,3 +99,5 @@ typedef Direction = Vector3;
 
 typedef Rotation = Quaternion;
 typedef RotationEuler = Vector3;
+
+typedef TransformMatrix = Matrix4;
