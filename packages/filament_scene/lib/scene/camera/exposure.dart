2b20ec5eb5d73409925f79a8bd2d1d23ae1c90ab
Jamie Kerber <kerber.jg@gmail.com>
2025-06-28 01:51:57 +0200

    [filament_scene] Defined new camera API primitives

--- packages/filament_scene/lib/scene/camera/exposure.dart
+++ packages/filament_scene/lib/scene/camera/exposure.dart
@@ -1,4 +1,3 @@
-part of 'camera.dart';
 
 /// An object that control camera Exposure.
 /// The exposure ultimately controls the scene's brightness,
@@ -10,22 +9,19 @@ part of 'camera.dart';
 class Exposure {
   /// Aperture in f-stops, clamped between 0.5 and 64. A lower aperture value increases the exposure,
   /// leading to a brighter scene. Realistic values are between 0.95 and 32.
-  double? aperture;
+  final double? aperture;
 
   /// shutterSpeed – Shutter speed in seconds,
   /// clamped between 1/25,000 and 60. A lower shutter speed increases the exposure.
   /// Realistic values are between 1/8000 and 30. sensitivity
-  double? shutterSpeed;
+  final double? shutterSpeed;
 
   /// Sensitivity in ISO, clamped between 10 and 204,800.
   /// A higher sensitivity increases the exposure. Realistic values are between 50 and 25600.
-  double? sensitivity;
+  final double? sensitivity;
 
   /// Sets this camera's exposure directly.
-  double? exposure;
-
-  ///Sets this camera's exposure (default is f/16, 1/125s, 100 ISO)
-  Exposure.formAperture({this.aperture, this.shutterSpeed, this.sensitivity});
+  final double? exposure;
 
   /// Sets this camera's exposure directly. Calling this method will set the aperture to 1.0,
   /// the shutter speed to 1.2 and the sensitivity will be computed to match the requested exposure
@@ -34,7 +30,48 @@ class Exposure {
   /// Many engines/tools use unit-less light intensities,
   /// which can be matched by setting the exposure manually.
   /// This can be typically achieved by setting the exposure to 1.0.
-  Exposure.formExposure({required this.exposure});
+  const Exposure([
+    final double exposure = 1.0,
+  ]) :
+    aperture = null,
+    shutterSpeed = null,
+    sensitivity = null,
+    // ignore: prefer_initializing_formals
+    exposure = exposure;
+
+  ///Sets this camera's exposure (default is f/16, 1/125s, 100 ISO)
+  const Exposure.fromAperture({
+    final double? aperture = 16.0,
+    final double? shutterSpeed = 1 / 125.0,
+    final double? sensitivity = 100.0,
+  }) :
+    exposure = null,
+    // ignore: prefer_initializing_formals
+    aperture = aperture,
+    assert(aperture != null && (aperture >= 0.5 && aperture <= 64), "Aperture must be between 0.5 and 64"),
+    // ignore: prefer_initializing_formals
+    shutterSpeed = shutterSpeed,
+    assert(shutterSpeed != null && (shutterSpeed >= 1 / 25000 && shutterSpeed <= 60), "Shutter speed must be between 1/25000 and 60 seconds"),
+    // ignore: prefer_initializing_formals
+    sensitivity = sensitivity,
+    assert(sensitivity != null && (sensitivity >= 10 && sensitivity <= 204800), "Sensitivity must be between 10 and 204800 ISO");
+
+  Exposure copyWith({
+    final double? aperture,
+    final double? shutterSpeed,
+    final double? sensitivity,
+    final double? exposure,
+  }) {
+    if (exposure != null) {
+      return Exposure(exposure);
+    } else {
+      return Exposure.fromAperture(
+        aperture: aperture ?? this.aperture,
+        shutterSpeed: shutterSpeed ?? this.shutterSpeed,
+        sensitivity: sensitivity ?? this.sensitivity,
+      );
+    }
+  }
 
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
--- packages/filament_scene/lib/scene/camera/lens_projection.dart
+++ packages/filament_scene/lib/scene/camera/lens_projection.dart
@@ -1,24 +1,46 @@
-part of 'camera.dart';
 
 ///An object that control camera and set it's projection matrix from the focal length.
 class LensProjection {
   /// lens's focal length in millimeters.
-  double focalLength;
+  final double focalLength;
 
   ///aspect ratio width/height.
-  double? aspect;
+  final double? aspect;
 
   ///distance in world units from the camera to the near plane.
   /// The near plane's position in view space is z = -near.
   /// Precondition: near > 0 for ProjectionType.PERSPECTIVE or near != far for ProjectionType.ORTHO.
-  double? near;
+  final double? near;
 
   ///distance in world units from the camera to the far plane.
   /// The far plane's position in view space is z = -far.
   /// Precondition: far > near for ProjectionType.PERSPECTIVE or far != near for ProjectionType.ORTHO.
-  double? far;
+  final double? far;
 
-  LensProjection({required this.focalLength, this.aspect, this.near, this.far});
+  const LensProjection({
+    this.focalLength = 50.0, // default focal length in mm
+    this.aspect,
+    this.near,
+    this.far,
+  }) :
+    assert(focalLength > 0, "Focal length must be greater than 0"),
+    assert(aspect == null || aspect > 0, "Aspect ratio must be greater than 0"),
+    assert(near == null || near > 0, "Near must be greater than 0 for perspective projection");
+    // assert(far == null || far > near!, "Far must be greater than near for perspective projection");
+
+  LensProjection copyWith({
+    final double? focalLength,
+    final double? aspect,
+    final double? near,
+    final double? far,
+  }) {
+    return LensProjection(
+      focalLength: focalLength ?? this.focalLength,
+      aspect: aspect ?? this.aspect,
+      near: near ?? this.near,
+      far: far ?? this.far,
+    );
+  }
 
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
